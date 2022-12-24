1f9d6268320f25383ef0e357f8fd32165a413292
kpeddakotla <kpeddakotla@gmail.com>
2022-12-23 21:49:44 -0800

    Initial commit

---
+++ DesktopAI/main.py
@@ -0,0 +1,48 @@
+import openai
+import speech_recognition as sr
+import pyttsx3
+
+openai.api_key = "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"
+key_phrase = "OK Google"
+
+
+def virtual_assistant():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    input_text = r.recognize_google(audio)
+    print(f"You said: {input_text}")
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=input_text,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5
+    ).choices[0].text
+
+    eng = pyttsx3.init()
+    voice = eng.getProperty('voices')
+    eng.setProperty('voice', voice[1].id)
+    print(response)
+    eng.say(response)
+    eng.runAndWait()
+
+
+while True:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        if r.recognize_google(audio) == key_phrase:
+            virtual_assistant()
+    except sr.UnknownValueError:
+        pass
+    except sr.RequestError as e:
+        print("Error:", e)
